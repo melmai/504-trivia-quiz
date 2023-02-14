d698e3491a473144d8031946b02a49e24998185d
Mel <mel@mel.codes>
2023-02-13 20:22:13 -0800

    adds ShortAnswerQuestion class with method stubs

---
+++ short_answer_question.py
@@ -0,0 +1,24 @@
+from true_false_question import TrueFalseQuestion
+
+
+class ShortAnswerQuestion(TrueFalseQuestion):
+    def __init__(self):
+        super().__init__()
+        options: []
+
+    def check_response(self, response):
+        """
+        This method checks the response of the user against the actual answer.
+        :param response: String
+        :return: Boolean
+        """
+        pass
+
+    def normalize(self, response):
+        """
+        This method formats the user's response for comparing against the
+        answer.
+        :param response: Original response from the user
+        :return: Reformatted string
+        """
+        pass
