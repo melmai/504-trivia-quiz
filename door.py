7118fafd0b30f3adc5e9fb91f1af02fe6b02ec33
Mel <mel@mel.codes>
2023-02-13 19:10:19 -0800

    adds Door class with method stubs

---
+++ door.py
@@ -0,0 +1,19 @@
+class Door:
+    def __init__(self):
+        pass
+
+    def unlock(self):
+        """
+        This method removes the barrier that prevents movement to the
+        next room.
+        """
+        pass
+
+    def check_answer(self, response):
+        """
+        This method checks the user provided response against the actual
+        answer.
+        :param response: String provided by user
+        :return: Boolean
+        """
+        pass
