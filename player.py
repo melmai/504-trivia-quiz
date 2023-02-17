c2d787f585d803becf2ce5e39151f08d45753d33
Mel <mel@mel.codes>
2023-02-17 14:30:23 -0800

    adds check_inventory() function

--- player.py
+++ player.py
@@ -19,3 +19,18 @@ class Player:
     def use_key(self):
         """This method decrements the number of keys in the inventory."""
         pass
+
+    def check_inventory(self):
+        if not self._keys:
+            inventory = "Sorry, no keys available!"
+        elif self._keys == 1:
+            inventory = "Ah, still have 1 key available. Better use it wisely."
+        else:
+            inventory = f"Phew. Still have {self._keys} keys in your pocket."
+
+        print(inventory)
+
+
+if __name__ == "__main__":
+    player = Player("Tom")
+    player.check_inventory()
