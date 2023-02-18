575f6af09f5bfb23229f536320f4d2dd23fc6e3e
Mel <mel@mel.codes>
2023-02-17 16:16:16 -0800

    adds normalize() and check_answer() methods

--- true_false_question.py
+++ true_false_question.py
@@ -1,7 +1,7 @@
 class TrueFalseQuestion:
-    def __init__(self):
-        self._question = None
-        self._answer = None
+    def __init__(self, question, answer):
+        self._question = question
+        self._answer = answer
 
     @property
     def question(self):
@@ -10,3 +10,26 @@ class TrueFalseQuestion:
     @property
     def answer(self):
         return self._answer
+
+    def normalize(self, response):
+        """
+        This method standardizes the user's response to compare against the
+        correct answer.
+        :param response:
+        :return:
+        """
+        response = response.lower().strip()
+        if response == "t" or response == "true":
+            return True
+        elif response == "f" or response == "false":
+            return False
+        else:
+            return None
+
+    def check_answer(self, response):
+        """
+        This method checks the user's response to determine if it is correct
+        :param response: user's response
+        :return: Boolean
+        """
+        return self.normalize(response) == self._answer
---
+++ true_false_question_test.py
@@ -0,0 +1,13 @@
+import unittest
+from true_false_question import TrueFalseQuestion
+
+
+class TrueFalseQuestionTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_check_answer(self):
+        pass
+
+    def test_normalize(self):
+        pass
