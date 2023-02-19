a3173a904eba93243adf38e8c76ac743397b709a
madisonstehle <mnstehle3@gmail.com>
2023-02-18 23:46:59 -0800

    cleaned up some code

--- maze.py
+++ maze.py
@@ -117,23 +117,22 @@ class Maze:
         :param y:
         :return: Tuple
         """
-        can_move_west = (0 <= y-1 < self._size) and self._rooms[x][y - 1] is not None and self._rooms[x][y - 1].can_move_to()
-        can_move_east = (0 <= y+1 < self._size) and self._rooms[x][y + 1] is not None and self._rooms[x][y + 1].can_move_to()
-        can_move_north = (0 <= x-1 < self._size) and self._rooms[x - 1][y] is not None and self._rooms[x - 1][y].can_move_to()
-        can_move_south = (0 <= x+1 < self._size) and self._rooms[x + 1][y] is not None and self._rooms[x + 1][y].can_move_to()
+        can_move_west = (0 <= y - 1 < self._size) and self._rooms[x][y - 1] is not None and self._rooms[x][y - 1].can_move_to()
+        can_move_east = (0 <= y + 1 < self._size) and self._rooms[x][y + 1] is not None and self._rooms[x][y + 1].can_move_to()
+        can_move_north = (0 <= x - 1 < self._size) and self._rooms[x - 1][y] is not None and self._rooms[x - 1][y].can_move_to()
+        can_move_south = (0 <= x + 1 < self._size) and self._rooms[x + 1][y] is not None and self._rooms[x + 1][y].can_move_to()
 
         return can_move_north, can_move_south, can_move_west, can_move_east
 
-# Below lines for local tests
-valid = False
-m = None
-while not valid:
-    m = Maze(4)
-    if m.is_traversable(0, 0):
-        print("Valid maze created!")
-        valid = True
-    else:
-        print("exit not reachable, making a new maze...")
 
-
-m.print_maze()
+# ===== Below lines are for local quick tests =====
+# valid = False
+# m = None
+# while not valid:
+#     m = Maze(4)
+#     if m.is_traversable(0, 0):
+#         print("Valid maze created!")
+#         valid = True
+#     else:
+#         print("exit not reachable, making a new maze...")
+# m.print_maze()
