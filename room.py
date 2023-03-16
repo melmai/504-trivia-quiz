5bb9cc7ec9e8e48b68e2330d798c9c272c512a95
melmai <mel@mel.codes>
2023-03-15 23:19:26 -0700

    updates room properties and methods to be private

--- room.py
+++ room.py
@@ -6,56 +6,56 @@ import random
 
 class Room:
     def __init__(self, row, col):
-        self._impassable = False
-        self._visited = False
-        self._row = row
-        self._col = col
-        self._is_exit = False
-        self._is_entrance = False
-        self._has_key = self.generate_key(random.randint(1, 100))
-        self._doors = {
+        self.__impassable = False
+        self.__visited = False
+        self.__row = row
+        self.__col = col
+        self.__is_exit = False
+        self.__is_entrance = False
+        self.__has_key = self.__generate_key(random.randint(1, 100))
+        self.__doors = {
             "north": False,
             "east": False,
             "west": False,
             "south": False
         }
-        self._active_door = None
+        self.__active_door = None
 
     @property
     def row(self):
-        return self._row
+        return self.__row
 
     @property
     def col(self):
-        return self._col
+        return self.__col
 
     @property
     def key(self):
-        return self._has_key
+        return self.__has_key
 
     @property
     def active_door(self):
-        return self._active_door
+        return self.__active_door
 
     @property
     def exit(self):
-        return self._is_exit
+        return self.__is_exit
 
-    def generate_key(self, key_chance):
+    def __generate_key(self, key_chance):
         """
         This method determines if the current room contains a key based
         on the likelihood of generating a key object.
         :param key_chance: chance of the room having a key object
         :return: Boolean
         """
-        if self.can_move_to() and not self._is_entrance and not self._is_exit:
+        if self.can_move_to() and not self.__is_entrance and not self.__is_exit:
             return key_chance >= 95
         else:
             return False
 
     def transfer_key(self):
         """This method removes the key from the room"""
-        self._has_key = False
+        self.__has_key = False
 
     def __str__(self):
         return self.construct_room_string()
@@ -67,32 +67,32 @@ class Room:
         :return: String
         """
 
-        if self._is_exit:
+        if self.__is_exit:
             content = "🏁"
-        elif self._is_entrance:
+        elif self.__is_entrance:
             content = "🎬"
-        elif self._has_key:
+        elif self.__has_key:
             content = "🗝"
         else:
             content = "  "
 
         room_str = ""
 
-        if self._doors["north"]:
+        if self.__doors["north"]:
             room_str += "*   *\n"
         else:
             room_str += "* * *\n"
 
-        if self._doors["east"] and self._doors["west"]:
+        if self.__doors["east"] and self.__doors["west"]:
             room_str += f"  {content} \n"
-        elif not self._doors["east"] and self._doors["west"]:
+        elif not self.__doors["east"] and self.__doors["west"]:
             room_str += f"  {content}*\n"
-        elif self._doors["east"] and not self._doors["west"]:
+        elif self.__doors["east"] and not self.__doors["west"]:
             room_str += f"* {content} \n"
         else:
             room_str += f"* {content}*\n"
 
-        if self._doors["south"]:
+        if self.__doors["south"]:
             room_str += "*   *\n"
         else:
             room_str += "* * *\n"
@@ -105,7 +105,7 @@ class Room:
         :param: None
         :return: None
         """
-        self._is_entrance = True
+        self.__is_entrance = True
 
     def get_is_exit(self):
         """
@@ -113,7 +113,7 @@ class Room:
         :param: None
         :return: Boolean
         """
-        return self._is_exit
+        return self.__is_exit
 
     def set_exit(self):
         """
@@ -121,7 +121,7 @@ class Room:
         :param: None
         :return: None
         """
-        self._is_exit = True
+        self.__is_exit = True
 
     def set_impassable(self, is_impassable):
         """
@@ -129,7 +129,7 @@ class Room:
         :param: is_impassable
         :return: None
         """
-        self._impassable = is_impassable
+        self.__impassable = is_impassable
 
     def can_move_to(self):
         """
@@ -138,7 +138,7 @@ class Room:
         :param: None
         :return: Boolean
         """
-        return not self._impassable
+        return not self.__impassable
 
     def set_visited(self, visited):
         """
@@ -146,7 +146,7 @@ class Room:
         :param: visited
         :return: None
         """
-        self._visited = visited
+        self.__visited = visited
 
     def can_enter(self):
         """
@@ -154,7 +154,7 @@ class Room:
         :param: None
         :return: Boolean
         """
-        return not self._impassable and not self._visited
+        return not self.__impassable and not self.__visited
 
     def get_door(self, direction):
         """
@@ -162,7 +162,7 @@ class Room:
         :param direction: String that represents the door position in the room
         :return: Door object or False if no door exists
         """
-        return self._doors[direction]
+        return self.__doors[direction]
 
     def set_door(self, direction, door=None):
         """
@@ -171,7 +171,7 @@ class Room:
         :param door: Door object to add.
         """
         door = door if door else Door()
-        self._doors[direction] = door
+        self.__doors[direction] = door
         return door
 
     def set_active_door(self, direction):
@@ -179,7 +179,7 @@ class Room:
         This method sets focus on an existing door of the room
         :param direction: string representing door to activate
         """
-        self._active_door = self._doors[direction]
+        self.__active_door = self.__doors[direction]
 
     def unlock_door(self):
         """
@@ -187,8 +187,8 @@ class Room:
         :return: True if successfully unlocks the door or False if already
         unlocked
         """
-        if self._active_door.locked:
-            self._active_door.unlock()
+        if self.__active_door.locked:
+            self.__active_door.unlock()
             return True
         return False
 
@@ -202,13 +202,13 @@ class Room:
         :return: True if player is free to move, else False
         """
         can_move = False
-        door = self._doors[direction]
+        door = self.__doors[direction]
 
         if door and player.dev:
             return True
 
         if door:  # there's a door
-            self._active_door = door
+            self.__active_door = door
             is_locked, is_answerable = door.try_door()
 
             if not is_locked:
@@ -226,9 +226,9 @@ class Room:
                 print("Uh oh, the way is blocked and there are no keys at my "
                       "disposal.")
 
-            self._active_door = None
+            self.__active_door = None
 
         return can_move  # can't move this direction
 
     def has_answerable_door(self, direction):
-        return self._doors[direction] and self._doors[direction].answerable
+        return self.__doors[direction] and self.__doors[direction].answerable
