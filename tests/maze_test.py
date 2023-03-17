cfae045da7a1b3f0f01655b119ddad233fcd2440
madisonstehle <mnstehle3@gmail.com>
2023-03-17 11:44:09 -0700

    created initialization tests for maze

--- tests/maze_test.py
+++ tests/maze_test.py
@@ -8,11 +8,13 @@ class MazeTest(unittest.TestCase):
 
     def test_initial_size(self):
         self.assertEqual(self.maze.size, 5)
-        self.assertEqual(len(self.maze.rooms), 4)
+        self.assertEqual(len(self.maze.rooms), 5)
 
     def test_entrance_exit_placement(self):
         self.assertEqual(self.maze.entrance, (0,0))
         self.assertEqual(self.maze.exit, (4,4))
+
+    def test_something(self):
 
 
 if __name__ == '__main__':
