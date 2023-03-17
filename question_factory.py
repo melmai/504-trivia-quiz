6b310007357a0bc239ea8d4912ac968b4b3ac6c9
William Chen <wc1321@uw.edu>
2023-03-17 11:38:01 -0700

    removed unnecessary stuff from question_factory

--- question_factory.py
+++ question_factory.py
@@ -55,72 +55,7 @@ class QuestionFactory:
             else:
                 raise ValueError('error no more questions')
 
-        # # read contents of db and put it inside the list, can be 1 or 3
-        # conn = sqlite3.connect('questions.db')
-        # c = conn.cursor()
-        # if question_type is None:
-        #     question_types = ["TrueFalse", "ShortAnswer", "MultipleChoice"]
-        #     question_type = random.choice(question_types)
-        # # keep track of which questions have been asked. grab all questions from db and add to a list. shuffle list
-        # if question_type == "TrueFalse":
-        #     c.execute("SELECT DISTINCT Question, Choices, Answer FROM TFquestions")
-        # elif question_type == "ShortAnswer":
-        #     c.execute("SELECT DISTINCT Question, Choices, Answer FROM SAquestions")
-        # elif question_type == "MultipleChoice":
-        #     c.execute("SELECT DISTINCT Question, Choices, Answer FROM MCquestions")
-        #
-        # row = c.fetchall()
-        # #fetchone is the reason I keep getting the same question.
-        # if not row:
-        #     raise ValueError(f"No rows returned for question type {question_type}")
-        # question_data = tuple(row)
-        # question = QuestionFactory.get_question(question_type, *question_data) #not good oo
-        # return question
 
-    # @staticmethod
-    # def get_question_data(question_type):
-    #     """
-    #     This method fetches the data for the provided question type
-    #     :param question_type: String representing one of the question types
-    #     :return: question data for the question type
-    #     """
-    #     correct_answer = False
-    #     answer_comment = "Joint Photographic Experts Group"
-    #     question = "The initials JPEG stand for Jagged Point Enabled Graphs"
-    #
-    #     return question, correct_answer, answer_comment
-
-    @staticmethod
-    def get_question(question_type, *data):
-        """
-        This method creates a Question object based on the type and data
-        provided.
-        :param question_type: "TrueFalse", "ShortAnswer", or "MultipleChoice"
-        :param data: optional data to add
-        :return: Question object
-        """
-
-        if question_type == "TrueFalse":
-            return TrueFalseQuestion(data[0], data[2])
-        elif question_type == "ShortAnswer":
-            return ShortAnswerQuestion(data[0], data[2])
-        elif question_type == "MultipleChoice":
-            choices = data[1].split(",")
-            return MultipleChoiceQuestion(data[0], choices, data[2])
-        else:
-            raise TypeError("That is not a valid question type.")
-        # if question_type == "TrueFalse":
-        #     return TrueFalseQuestion(*data)
-        # elif question_type == "ShortAnswer":
-        #     return ShortAnswerQuestion(*data)
-        # elif question_type == "MultipleChoice":
-        #     return MultipleChoiceQuestion(*data)
-        # else:
-        #     raise TypeError("That is not a valid question type.")
-        #
-        # return TrueFalseQuestion(*data)
-
-    # print(QuestionFactory.generate_question("TrueFalse"))
 
     @staticmethod
     def mock(question_type):
