45127b5e96ddb9bf563345181873c5a19b3fd4e7
Mel <mel@mel.codes>
2023-03-17 18:38:12 -0700

    updates formatting for PEP 8

--- pickle_test.py
+++ pickle_test.py
@@ -1,26 +1,27 @@
-import trivia_quiz
 from maze import Maze
 from player import Player
 from trivia_quiz import TriviaQuiz
 import unittest
-import sys
 import pickle
-t = TriviaQuiz
 
-testsave = 'test_save.pickle'
-class save_test(unittest.TestCase):
+
+class SaveTest(unittest.TestCase):
+
+    def setUp(self):
+        self.trivia_quiz = TriviaQuiz()
+        self.save_file = 'test_save.pickle'
+
     def test_save(self):
         """This method tests the ability to pickle a maze and player """
-        testsave = 'test_save.pickle'
         maze = Maze(2)
         player = Player("testman", keys=15)
-        t.save_game(testsave, maze, player)
+        self.trivia_quiz.save_game(self.save_file, maze, player)
 
     def test_load(self):
-        """This method tests the ability to load a game and that the attributes are correct """
+        """This method tests the ability to load a game and that the attributes
+        are correct """
 
-        testsave = 'test_save.pickle'
-        with open (testsave, 'rb') as f:
+        with open(self.save_file, 'rb') as f:
             save_data = pickle.load(f)
 
         expected_size = 2
@@ -29,4 +30,3 @@ class save_test(unittest.TestCase):
         self.assertEqual(save_data['maze'].size, expected_size)
         self.assertEqual(save_data['player'].name, expected_name)
         self.assertEqual(save_data['player'].keys, expected_keys)
-
